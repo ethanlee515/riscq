dcb364e70fb15c699d7e2bb450d72b4b6839ba3f
Yi Lee <ylee1228@umd.edu>
2025-03-29 23:44:54 -0400

    Attempt to build and load ELF

--- src/main/scala/riscq/tester/MMSocAluTestBench.scala
+++ src/main/scala/riscq/tester/MMSocAluTestBench.scala
@@ -10,15 +10,10 @@ object TestAlu extends App {
   val simConfig = SimConfig.addSimulatorFlag("-Wno-MULTIDRIVEN")
   simConfig.compile(MemoryMapSoc(withWhitebox = true)).doSim { dut =>
     val driver = new MMSocDriver(dut)
-    val asm = new RvAssembler(32)
+    driver.loadInsts()
     driver.init()
     driver.rstUp()
-    val insts = List(
-      asm.addi(1, 0, 3),
-      asm.add(2, 1, 2),
-      asm.beq(0, 0, -2)
-    )
-    driver.loadInsts(insts)
+
     dut.riscq_rst #= true
     for(_ <- 0 until 10) {
       driver.tick()
--- src/main/scala/riscq/tester/MMSocDriver.scala
+++ src/main/scala/riscq/tester/MMSocDriver.scala
@@ -5,10 +5,18 @@ import spinal.core.sim._
 import spinal.lib._
 import riscq.soc.MemoryMapSoc
 import riscq.test.WhiteboxerPlugin
+import spinal.lib.misc.Elf
+import java.io.File
 
 class MMSocDriver(dut: MemoryMapSoc) {
   val cd = dut.clockDomain
   val wb = dut.core.host[WhiteboxerPlugin].logic
+
+  def loadInsts() = {
+    val elfFile = new File("src/main/asm/testAlu.elf")
+    val elf = new Elf(elfFile, addressWidth = 32)
+    elf.load(dut.mem.mem, -0x80000000)
+  }
 
   def init() = {
     cd.forkStimulus(10)
@@ -20,15 +28,6 @@ class MMSocDriver(dut: MemoryMapSoc) {
 
   def tick() = {
     cd.waitRisingEdge(1)
-  }
-
-  def loadInsts(insts: Seq[String]) = {
-    var addr : Long = 0
-    for (inst <- insts) {
-      val instInt = BigInt(inst, 2)
-      dut.mem.mem.setBigInt(addr, instInt)
-      addr += 1
-    }
   }
 
   def logRf() = {
